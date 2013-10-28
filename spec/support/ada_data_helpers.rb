824c39aa59daeb38559384c5c991254f994f4429
Isaac Goodin <isaacgoodin@gmail.com>
2013-10-28 10:32:01 -0500

    Fixed a bug with the User IAT count query returning false even if there were results

--- spec/support/ada_data_helpers.rb
+++ spec/support/ada_data_helpers.rb
@@ -1,10 +1,10 @@
 module ADADataHelpers
   def AddPreIAT(user)
-    AdaData.where({gameName: ENV['GameName_Pre'],user_id: user.ada_id, key: ENV['IAT_Flag']}).create
+    AdaData.create({gameName: ENV['GameName_Pre'],user_id: user.ada_id, key: ENV['IAT_Flag']})
   end
 
   def AddPostIAT(user)
-    AdaData.where({gameName: ENV['GameName_Post'],user_id: user.ada_id, key: ENV['IAT_Flag']}).create
+    AdaData.create({gameName: ENV['GameName_Post'],user_id: user.ada_id, key: ENV['IAT_Flag']})
   end
 
   def FinishedIATs(user)
